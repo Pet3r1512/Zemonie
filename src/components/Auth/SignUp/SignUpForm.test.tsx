b4efc984068e3cdaee958478c85872f6cbf1fed4
Peter Pham <pttp15122002@gmail.com>
2026-05-20 12:08:00 +1000

    test: for passwords without number

--- src/components/Auth/SignUp/SignUpForm.test.tsx
+++ src/components/Auth/SignUp/SignUpForm.test.tsx
@@ -257,4 +257,20 @@ describe("Password validation", () => {
       ).toBeInTheDocument();
     });
   });
+
+  it("shows error when the password has no number", async () => {
+    renderForm();
+
+    await userEvent.type(screen.getByRole("password"), "passwordwithoutnumber");
+
+    fireEvent.submit(screen.getByRole("form"));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          "Minimum 8 characters, at least one letter and one number",
+        ),
+      ).toBeInTheDocument();
+    });
+  });
 });
