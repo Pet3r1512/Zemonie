1fc73d5232913e7b9dda2cbce4f13ca2af0dcaf6
Peter Pham <pttp15122002@gmail.com>
2026-05-20 20:15:57 +1000

    ops: api endpoint for getting income growth

---
+++ src/api/users/analytics/getIncomeGrowth.ts
@@ -0,0 +1,26 @@
+import { SERVER_URL } from "@/constant/auth";
+
+export default async function getIncomeGrowth(input?: { userId: string }) {
+  const encodedInput = encodeURIComponent(JSON.stringify(input ?? {}));
+
+  const response = await fetch(
+    `${SERVER_URL}/api/trpc/analytics.incomeGrowth?input=${encodedInput}`,
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  const res = await response.json();
+
+  if (!response.ok) {
+    throw new Error(res.error?.message ?? "Unknown Error");
+  }
+
+  return {
+    incomeGrowthRate: res.result.data,
+  };
+}
