86dc0026f9650c932f1ed7dcdf22f60a3e2d8623
Peter Pham <pttp15122002@gmail.com>
2026-05-22 23:15:25 +1000

    style

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header
       role="header"
-      className="flex justify-between items-center max-w-7xl mx-auto px-5 lg:px-0 py-5 md:py-6 lg:py-7"
+      className="w-full flex justify-between items-center max-w-7xl mx-auto px-5 lg:px-0 py-10 lg:py-5"
     >
       <Logo />
       <Navbar />
