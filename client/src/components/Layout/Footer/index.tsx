b368c5acf7d5256a9877defc2b4b060f16ff050a
Peter Pham <pttp15122002@gmail.com>
2026-05-22 23:18:48 +1000

    add(footer): logo and slogan

--- client/src/components/Layout/Footer/index.tsx
+++ client/src/components/Layout/Footer/index.tsx
@@ -2,9 +2,16 @@ import Logo from "../Logo";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#111827] w-screen! mt-auto">
+    <footer className="bg-[#111827] w-screen! mt-auto text-white">
       <div className="footer-container max-w-7xl mx-auto py-10 px-5 lg:px-0">
-        <Logo />
+        <div className="flex flex-col gap-y-5 w-1/4">
+          <Logo />
+          <p>
+            Smart money management for{" "}
+            <strong className="text-primary">GenZ</strong>. Take control of your
+            finances today.
+          </p>
+        </div>
       </div>
     </footer>
   );
