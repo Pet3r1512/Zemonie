b0a4bffa03bdf6d2f7e84950e708ca17dc69063f
Peter Pham <pttp15122002@gmail.com>
2026-05-22 14:35:09 +1000

    add: a loading screen

---
+++ client/src/components/Layout/LoadingScreen.tsx
@@ -0,0 +1,25 @@
+export default function LoadingScreen() {
+  const words = "Loading...".split("");
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center">
+        <img
+          src="/logo/zemonie-icon-light.svg"
+          alt="Zemonie Icon"
+          className="w-auto h-10 md:h-14 lg:h-20"
+        />
+        <div className="flex space-x-1 justify-center items-center py-10">
+          {words.map((letter, index) => (
+            <span
+              key={index}
+              className="text-xl font-bold animate-bounce"
+              style={{ animationDelay: `${index * 0.1}s` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
