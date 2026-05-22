3f0c4acac285760aac1e0393ac953df5588d71ef
Peter Pham <pttp15122002@gmail.com>
2026-05-22 16:53:40 +1000

    fix: missing month and year

--- client/src/hooks/useFetchCurrentMonthIncome.ts
+++ client/src/hooks/useFetchCurrentMonthIncome.ts
@@ -1,12 +1,14 @@
 import getTotalIncomeByMonth from "@/api/users/analytics/income/getTotalIncomeByMonth";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useFetchCurrentMonthIncome({ userId }: { userId: string }) {
+export default function useFetchCurrentMonthIncome({ userId, month, year }: { userId: string, month?: number, year?: number }) {
     const { data, isLoading, isError } = useQuery({
         queryKey: ["totalIncome", userId],
         queryFn: () =>
             getTotalIncomeByMonth({
                 userId: userId!,
+                month: month,
+                year: year
             }),
         enabled: !!userId,
         refetchOnWindowFocus: false,
