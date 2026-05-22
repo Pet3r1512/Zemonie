df5eefe5f836ce73c3769118ccb8972c2f759765
Peter Pham <pttp15122002@gmail.com>
2026-05-22 23:15:48 +1000

    style: place footer at the bottom of page

--- client/src/components/Layout/Footer/index.tsx
+++ client/src/components/Layout/Footer/index.tsx
@@ -2,7 +2,7 @@ import Logo from "../Logo";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#111827] w-screen!">
+    <footer className="bg-[#111827] w-screen! mt-auto">
       <div className="footer-container max-w-7xl mx-auto py-10 px-5 lg:px-0">
         <Logo />
       </div>
