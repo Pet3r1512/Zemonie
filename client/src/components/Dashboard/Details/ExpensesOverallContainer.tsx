50cbd7b75aec2eb50ed276ada72faa11bc854ed6
Peter Pham <pttp15122002@gmail.com>
2026-05-22 12:57:58 +1000

    feat: display highest expense

--- client/src/components/Dashboard/Details/ExpensesOverallContainer.tsx
+++ client/src/components/Dashboard/Details/ExpensesOverallContainer.tsx
@@ -4,6 +4,7 @@ import Data from "../Overall/Data";
 import { useQuery } from "@tanstack/react-query";
 import getTotalExpensesByMonth from "@/api/users/analytics/expenses/getTotalExpensesByMonth";
 import useFetchUser from "@/hooks/useFetchUser";
+import getHighestExpenseCategory from "@/api/users/analytics/expenses/getHighestExpenseCategory";
 
 export default function ExpensesOverallContainer() {
   const userId = useFetchUser();
@@ -11,6 +12,15 @@ export default function ExpensesOverallContainer() {
   const totalExpenseQuery = useQuery({
     queryKey: ["totalExpenses"],
     queryFn: () => getTotalExpensesByMonth({ userId: userId! }),
+    enabled: !!userId,
+    refetchOnWindowFocus: false,
+    staleTime: 5 * 60 * 1000,
+    gcTime: 30 * 60 * 1000,
+  });
+
+  const highestExpenseQuery = useQuery({
+    queryKey: ["highestExpense"],
+    queryFn: () => getHighestExpenseCategory({ userId: userId! }),
     enabled: !!userId,
     refetchOnWindowFocus: false,
     staleTime: 5 * 60 * 1000,
@@ -33,15 +43,15 @@ export default function ExpensesOverallContainer() {
     },
     {
       name: "Highest Category",
-      subtitle: "",
+      subtitle: highestExpenseQuery.data?.categoryName,
       icon: (
         <div className="flex items-center justify-center rounded-full p-2.5 bg-purple-100">
           <ChartPie className="text-purple-600" />
         </div>
       ),
-      isLoading: false,
-      isError: false,
-      amount: 0,
+      isLoading: highestExpenseQuery.isLoading,
+      isError: highestExpenseQuery.isError,
+      amount: highestExpenseQuery.data?.highestExpenseCategoryAmount,
     },
     {
       name: "Expense Growth",
