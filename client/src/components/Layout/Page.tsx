3164a1c59707792ba52049b5fa3282249cd99f97
Peter Pham <pttp15122002@gmail.com>
2026-05-22 23:14:25 +1000

    style: make sure footer is always at the bottom of page

--- client/src/components/Layout/Page.tsx
+++ client/src/components/Layout/Page.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { cn } from "../../lib/utils";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Page({
   children,
@@ -12,16 +13,27 @@ export default function Page({
   pageName?: string;
 }) {
   return (
-    <main className="body bg-cover bg-center h-full">
+    <main className="body bg-cover bg-center min-h-screen flex flex-col">
       <Header />
+
       <section
         role="page-body"
-        className={cn("flex flex-col mx-auto max-w-360", pageName)}
+        className={cn(
+          "flex-1 flex flex-col mx-auto w-full max-w-360",
+          pageName,
+        )}
       >
-        <div className={cn("mx-auto w-full max-w-7xl px-5 lg:px-0", className)}>
+        <div
+          className={cn(
+            "mx-auto w-full max-w-7xl px-5 lg:px-0 flex-1",
+            className,
+          )}
+        >
           {children}
         </div>
       </section>
+
+      <Footer />
     </main>
   );
 }
