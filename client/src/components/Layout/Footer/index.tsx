0c589d9c1839700906d3e4df7a1e4aaa229e56b5
Peter Pham <pttp15122002@gmail.com>
2026-05-22 23:01:31 +1000

    add: footer component

---
+++ client/src/components/Layout/Footer/index.tsx
@@ -0,0 +1,11 @@
+import Logo from "../Logo";
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#111827] w-screen!">
+      <div className="footer-container max-w-7xl mx-auto py-10 px-5 lg:px-0">
+        <Logo />
+      </div>
+    </footer>
+  );
+}
