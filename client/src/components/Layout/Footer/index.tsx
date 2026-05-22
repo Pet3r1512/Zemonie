1216a8977356637353ac3c7d9f26dd91f010136f
Peter Pham <pttp15122002@gmail.com>
2026-05-22 23:36:39 +1000

    add: common links section to footer

--- client/src/components/Layout/Footer/index.tsx
+++ client/src/components/Layout/Footer/index.tsx
@@ -1,9 +1,10 @@
 import Logo from "../Logo";
+import CommonLinks from "./CommonLinks";
 
 export default function Footer() {
   return (
     <footer className="bg-[#111827] w-screen! mt-auto text-white">
-      <div className="footer-container max-w-7xl mx-auto py-10 px-5 lg:px-0">
+      <div className="footer-container max-w-7xl mx-auto py-10 px-5 lg:px-0 flex gap-x-24">
         <div className="flex flex-col gap-y-5 w-1/4">
           <Logo />
           <p>
@@ -12,6 +13,7 @@ export default function Footer() {
             finances today.
           </p>
         </div>
+        <CommonLinks />
       </div>
     </footer>
   );
