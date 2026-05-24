d177944be8f4e1e54330f9411b91b519e316c562
Peter Pham <pttp15122002@gmail.com>
2026-05-24 16:25:40 +1000

    ops(database): update avatarId and currency

--- server/src/server/user.ts
+++ server/src/server/user.ts
@@ -1,3 +1,35 @@
-import { router } from "./tRPC";
+import z from "zod";
+import { publicProcedure, router } from "./tRPC";
+import { SupportedCurrency } from "@prisma/client";
+import prisma from "@/lib/prisma";
 
-export const userRouter = router({})
+export const userRouter = router({
+    setup: publicProcedure.input(z.object({
+        userId: z.string(),
+        avatarId: z.string(),
+        currency: SupportedCurrency
+    })).mutation(async ({ input }) => {
+        const { userId, avatarId, currency } = input
+
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                image: avatarId,
+
+            }
+        })
+
+        await prisma.balance.update({
+            where: {
+                userId: userId
+            },
+            data: {
+                currency: currency as SupportedCurrency
+            }
+        })
+
+        return { success: true }
+    })
+})
