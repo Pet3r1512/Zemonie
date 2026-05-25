0c8fa30fcc4bbd3eac6bac3029abadfba83a4604
Peter Pham <pttp15122002@gmail.com>
2026-05-25 11:56:22 +1000

    imp: use default cursor for coming soon badge

--- client/src/components/Auth/SignInViaGoogleBtn.tsx
+++ client/src/components/Auth/SignInViaGoogleBtn.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 export default function SignInViaGoogleBtn() {
   return (
     <div className="flex flex-col gap-4 relative z-1">
-      <p className="absolute -top-3 z-100 -right-4.5 text-white rounded-2xl font-semibold text-xs bg-blue-400 px-1.5 py-0.5">
+      <p className="absolute -top-3 z-100 -right-4.5 text-white rounded-2xl font-semibold text-xs bg-blue-400 px-1.5 py-0.5 cursor-default">
         Coming Soon
       </p>
       <Button type="button" disabled variant="outline" className="w-full">
