801c8060ab585552734126f29d079e44c8bf8df8
Peter Pham <pttp15122002@gmail.com>
2026-05-25 18:28:22 +1000

    add: release banner to homepage

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,8 +1,15 @@
+import { Release } from "@/components/Banner/Release";
 import { warmupDb } from "@/lib/ping";
-import { createRootRoute } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   beforeLoad: () => {
     warmupDb();
   },
+  component: () => (
+    <>
+      <Release />
+      <Outlet />
+    </>
+  ),
 });
