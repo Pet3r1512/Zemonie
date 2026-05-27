68009f7246f9394c38bad1a385c0c660106f3403
Peter Pham <pttp15122002@gmail.com>
2026-05-27 14:29:50 +1000

    fix: add ResizeObserver for tests

--- client/setupTests.ts
+++ client/setupTests.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+
+if (typeof ResizeObserver === "undefined") {
+  globalThis.ResizeObserver = class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  } as unknown as typeof ResizeObserver;
+}
