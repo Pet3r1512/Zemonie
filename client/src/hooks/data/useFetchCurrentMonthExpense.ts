9c91d85ccb50c62d9f9b2344a82ef5544d0ab7ac
Peter Pham <pttp15122002@gmail.com>
2026-06-03 16:55:32 +1000

    imp: create a reusable hook for fetching current expenses

---
+++ client/src/hooks/data/useFetchCurrentMonthExpense.ts
@@ -0,0 +1,18 @@
+import getTotalExpensesByMonth from "@/api/users/analytics/expenses/getTotalExpensesByMonth";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useFetchCurrentMonthExpenses({ month, year }: { month?: number, year?: number }) {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["totalExpenses"],
+        queryFn: () =>
+            getTotalExpensesByMonth({
+                month: month,
+                year: year
+            }),
+        refetchOnWindowFocus: false,
+        staleTime: 5 * 60 * 1000, // 5 mins
+        gcTime: 30 * 60 * 1000, // 30 mins
+    })
+
+    return { data, isLoading, isError }
+}
