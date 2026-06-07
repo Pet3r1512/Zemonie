57547378488c59e573e31922ff99e583a97194da
Peter Pham <pttp15122002@gmail.com>
2026-06-07 15:25:09 +1000

    fix: use istanbul provider instead

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./setupTests.ts"],
     coverage: {
-      provider: "v8",
+      provider: "istanbul",
       reporter: ["text"],
       exclude: [
         "src/stories/**",
