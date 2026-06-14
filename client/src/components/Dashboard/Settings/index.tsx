e8858658339fc473243cccdab6160d152fb81a0d
Peter Pham <pttp15122002@gmail.com>
2026-06-14 14:00:57 +1000

    add: setting container

---
+++ client/src/components/Dashboard/Settings/index.tsx
@@ -0,0 +1,20 @@
+import ThemeToggle from "@/components/ThemeToggle";
+import { Card } from "@/components/ui/card";
+import { UserRoundCog } from "lucide-react";
+
+export default function SettingsContainer() {
+  return (
+    <Card className="p-5">
+      <section className="space-y-5">
+        <div className="flex items-center gap-x-1 text-secondary">
+          <UserRoundCog />
+          <p className="lg:text-lg font-bold">Preferences</p>
+        </div>
+        <div className="flex items-center justify-between md:max-w-2xl px-10">
+          <p>Theme</p>
+          <ThemeToggle />
+        </div>
+      </section>
+    </Card>
+  );
+}
