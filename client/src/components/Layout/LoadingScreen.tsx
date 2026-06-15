5e5a98d0102f495e028bdd6a4c06e4061c0daef4
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-06-15 18:17:15 +1000

    #IMP-045: Loading screen (#214)

--- client/src/components/Layout/LoadingScreen.tsx
+++ client/src/components/Layout/LoadingScreen.tsx
@@ -1,25 +1,9 @@
+import { Atom } from "react-loading-indicators";
+
 export default function LoadingScreen() {
-  const words = "Loading...".split("");
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center">
-        <img
-          src="/logo/zemonie_icon.webp"
-          alt="Zemonie Icon"
-          className="w-auto h-10 md:h-14 lg:h-20"
-        />
-        <div className="flex space-x-1 justify-center items-center py-10">
-          {words.map((letter, index) => (
-            <span
-              key={index}
-              className="text-xl font-bold animate-bounce"
-              style={{ animationDelay: `${index * 0.1}s` }}
-            >
-              {letter}
-            </span>
-          ))}
-        </div>
-      </div>
+      <Atom color="#ff7900" size="medium" text="" textColor="" />
     </div>
   );
 }
