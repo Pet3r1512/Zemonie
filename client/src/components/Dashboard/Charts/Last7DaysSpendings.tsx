1a38f79c53a5b37f1e82416cf415e67cb9cab94c
Peter Pham <pttp15122002@gmail.com>
2026-06-15 18:29:54 +1000

    chore: default component from shadcn

--- client/src/components/Dashboard/Charts/Last7DaysSpendings.tsx
+++ client/src/components/Dashboard/Charts/Last7DaysSpendings.tsx
@@ -1,3 +1,55 @@
+"use client";
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
+
+export const description = "A bar chart";
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#f79d65",
+  },
+} satisfies ChartConfig;
+
 export default function Last7DaysSpendings() {
-  return <></>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Last 7 Days Spendings</CardTitle>
+      </CardHeader>
+      <CardContent className="min-h-110!">
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
 }
