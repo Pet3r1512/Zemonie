2edae7416e5dcb54fe4e6d943dce13d41af3d3fb
Peter Pham <pttp15122002@gmail.com>
2026-06-17 20:16:38 +1000

    add: icons for income category

--- client/src/components/Dashboard/Overall/Forms/Selectors/IncomeSelector.tsx
+++ client/src/components/Dashboard/Overall/Forms/Selectors/IncomeSelector.tsx
@@ -9,40 +9,52 @@ import {
 import { useFormContext } from "react-hook-form";
 import { Transaction } from "../IncomeForm";
 import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+import { Briefcase, Gift, Heart, Home, Laptop, PlusCircle, TrendingUp } from "lucide-react";
 
 type SourceIncome = {
   name: string;
   id: number;
+  icon: ReactNode;
 };
+
+const ICON_SIZE = 16;
 
 const incomesources: SourceIncome[] = [
   {
     name: "Salary",
     id: 1,
+    icon: <Briefcase size={ICON_SIZE} className="text-emerald-500" />,
   },
   {
     name: "Bonus",
     id: 2,
+    icon: <Gift size={ICON_SIZE} className="text-yellow-400" />,
   },
   {
     name: "Freelance",
     id: 3,
+    icon: <Laptop size={ICON_SIZE} className="text-violet-500" />,
   },
   {
     name: "Investment",
     id: 4,
+    icon: <TrendingUp size={ICON_SIZE} className="text-sky-500" />,
   },
   {
     name: "Retail Income",
     id: 5,
+    icon: <Home size={ICON_SIZE} className="text-orange-400" />,
   },
   {
     name: "Gift",
     id: 6,
+    icon: <Heart size={ICON_SIZE} className="text-pink-400" />,
   },
   {
     name: "Other",
     id: 7,
+    icon: <PlusCircle size={ICON_SIZE} className="text-teal-400" />,
   },
 ];
 
@@ -74,7 +86,10 @@ export default function IncomeSelect({
                 value={source.id.toString()}
                 className="lg:hover:bg-gray-200 dark:lg:hover:bg-dark-card"
               >
-                {source.name}
+                <div className="flex items-center gap-x-1">
+                  {source.icon}
+                  <p>{source.name}</p>
+                </div>
               </SelectItem>
             );
           })}
