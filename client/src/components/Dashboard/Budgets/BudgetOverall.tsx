811cc1c2788ce630e03af4e931eb4984011b78aa
Peter Pham <pttp15122002@gmail.com>
2026-06-19 19:59:14 +1000

    add: mock data

--- client/src/components/Dashboard/Budgets/BudgetOverall.tsx
+++ client/src/components/Dashboard/Budgets/BudgetOverall.tsx
@@ -1,3 +1,44 @@
 export default function BudgetOverall() {
   return <></>;
+  const budgetOverallData: OverallDataType[] = [
+    {
+      name: "Total Budget",
+      subtitle: "Monthly allocation",
+      icon: (
+        <div className="flex items-center justify-center rounded-full p-2.5 bg-blue-100 dark:bg-blue-900/30">
+          <Crosshair className="text-blue-500" />
+        </div>
+      ),
+      isLoading: false,
+      isError: false,
+      amount: 3200.0,
+      currency,
+    },
+    {
+      name: "Total Spent",
+      subtitle: "72% of budget",
+      icon: (
+        <div className="flex items-center justify-center rounded-full p-2.5 bg-red-100 dark:bg-red-900/30">
+          <TrendingUp className="text-red-500" />
+        </div>
+      ),
+      isLoading: false,
+      isError: false,
+      amount: 2310.0,
+      currency,
+    },
+    {
+      name: "Remaining",
+      subtitle: "Available to spend",
+      icon: (
+        <div className="flex items-center justify-center rounded-full p-2.5 bg-lime-100 dark:bg-lime-900/30">
+          <HandCoins className="text-lime-500" />
+        </div>
+      ),
+      isLoading: false,
+      isError: false,
+      amount: 3200.0,
+      currency,
+    },
+  ];
 }
