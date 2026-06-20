11174507e93044934097952fde40fba3634b03df
Peter Pham <pttp15122002@gmail.com>
2026-06-20 19:03:59 +1000

    fix: bug

--- client/src/components/ui/table.tsx
+++ client/src/components/ui/table.tsx
@@ -2,24 +2,24 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-const Table = ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
+const Table = ({ className, ...props }: React.ComponentProps<"table">) => (
   <div className="relative w-full overflow-auto">
     <table className={cn("w-full caption-bottom text-sm", className)} {...props} />
   </div>
 );
 Table.displayName = "Table";
 
-const TableHeader = ({ className, ...props }: React.HTMLAttributes<HTMLTableSectionElement>) => (
+const TableHeader = ({ className, ...props }: React.ComponentProps<"thead">) => (
   <thead className={cn("[&_tr]:border-b", className)} {...props} />
 );
 TableHeader.displayName = "TableHeader";
 
-const TableBody = ({ className, ...props }: React.HTMLAttributes<HTMLTableSectionElement>) => (
+const TableBody = ({ className, ...props }: React.ComponentProps<"tbody">) => (
   <tbody className={cn("[&_tr:last-child]:border-0", className)} {...props} />
 );
 TableBody.displayName = "TableBody";
 
-const TableFooter = ({ className, ...props }: React.HTMLAttributes<HTMLTableSectionElement>) => (
+const TableFooter = ({ className, ...props }: React.ComponentProps<"tfoot">) => (
   <tfoot
     className={cn(
       "border-t bg-oklch(0.97 0 0)/50 font-medium [&>tr]:last:border-b-0 dark:bg-oklch(0.269 0 0)/50",
@@ -30,7 +30,7 @@ const TableFooter = ({ className, ...props }: React.HTMLAttributes<HTMLTableSect
 );
 TableFooter.displayName = "TableFooter";
 
-const TableRow = ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
+const TableRow = ({ className, ...props }: React.ComponentProps<"tr">) => (
   <tr
     className={cn(
       "border-b border-neutral-200 transition-colors hover:bg-oklch(0.97 0 0)/50 data-[state=selected]:bg-oklch(0.97 0 0) dark:border-dark-elevated dark:hover:bg-oklch(0.269 0 0)/50 dark:data-[state=selected]:bg-oklch(0.269 0 0)",
@@ -41,7 +41,7 @@ const TableRow = ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElem
 );
 TableRow.displayName = "TableRow";
 
-const TableHead = ({ className, ...props }: React.ThHTMLAttributes<HTMLTableCellElement>) => (
+const TableHead = ({ className, ...props }: React.ComponentProps<"th">) => (
   <th
     className={cn(
       "h-12 px-4 text-left align-middle font-medium text-oklch(0.556 0 0) [&:has([role=checkbox])]:pr-0 dark:text-oklch(0.708 0 0)",
@@ -52,7 +52,7 @@ const TableHead = ({ className, ...props }: React.ThHTMLAttributes<HTMLTableCell
 );
 TableHead.displayName = "TableHead";
 
-const TableCell = ({ className, ...props }: React.TdHTMLAttributes<HTMLTableCellElement>) => (
+const TableCell = ({ className, ...props }: React.ComponentProps<"td">) => (
   <td
     className={cn("p-2.5 lg:p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
@@ -60,7 +60,7 @@ const TableCell = ({ className, ...props }: React.TdHTMLAttributes<HTMLTableCell
 );
 TableCell.displayName = "TableCell";
 
-const TableCaption = ({ className, ...props }: React.HTMLAttributes<HTMLTableCaptionElement>) => (
+const TableCaption = ({ className, ...props }: React.ComponentProps<"caption">) => (
   <caption
     className={cn("mt-4 text-sm text-oklch(0.556 0 0) dark:text-oklch(0.708 0 0)", className)}
     {...props}
