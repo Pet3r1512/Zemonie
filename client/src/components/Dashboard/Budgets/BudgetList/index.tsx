e820e1398a1d50b02e9fa7e9964c6b34ef22a413
Peter Pham <pttp15122002@gmail.com>
2026-06-21 17:21:52 +1000

    add: category item to budget container

--- client/src/components/Dashboard/Budgets/BudgetList/index.tsx
+++ client/src/components/Dashboard/Budgets/BudgetList/index.tsx
@@ -1,7 +1,10 @@
+import BudgetItem from "./BudgetItem";
+
 export default function BudgetListContainer() {
   return (
     <section className="bg-white dark:bg-dark-card rounded-2xl p-5 flex flex-col gap-y-5 flex-1">
       <p className="text-lg lg:text-xl font-bold shrink-0">Budget By Category</p>
+      <BudgetItem />
     </section>
   );
 }
