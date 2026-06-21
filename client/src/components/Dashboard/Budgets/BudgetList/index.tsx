40aee35a14f90e42c77b65fb5eca450156ed5b3b
Peter Pham <pttp15122002@gmail.com>
2026-06-21 19:22:32 +1000

    ops: define return type for budgets

--- client/src/components/Dashboard/Budgets/BudgetList/index.tsx
+++ client/src/components/Dashboard/Budgets/BudgetList/index.tsx
@@ -1,4 +1,27 @@
 import BudgetItem from "./BudgetItem";
+enum BudgetDuration {
+  WEEK_1,
+  WEEK_2,
+  MONTH_1,
+  MONTH_3,
+  MONTH_6,
+  MONTH_12,
+}
+
+export type BudgetResponseType = {
+  id: string;
+  amount: string;
+  categoryId: number;
+  createdAt: string;
+  updatedAt: string;
+  duration: BudgetDuration;
+  startDate: string;
+  endDate: string;
+  isRecurring: boolean;
+  isRollOver: boolean;
+  name: string;
+  parentBudgetId: string;
+};
 
 export default function BudgetListContainer() {
   return (
