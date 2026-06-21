729dc252bd8b4c3bb6987190cbc169b859aea52d
Peter Pham <pttp15122002@gmail.com>
2026-06-21 16:55:00 +1000

    add: budget list container

---
+++ client/src/components/Dashboard/Budgets/BudgetList/index.tsx
@@ -0,0 +1,7 @@
+export default function BudgetListContainer() {
+  return (
+    <section className="bg-white dark:bg-dark-card rounded-2xl p-5 flex flex-col gap-y-5 flex-1">
+      <p className="text-lg lg:text-xl font-bold shrink-0">Budget By Category</p>
+    </section>
+  );
+}
