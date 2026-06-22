5c8c44f1c953cf5fe7b91c131014b038765c943c
Peter Pham <pttp15122002@gmail.com>
2026-06-22 22:58:06 +1000

    style: remove border

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -12,7 +12,7 @@ export default function Header({ hideHeader }: { hideHeader: boolean }) {
         "fixed top-0 left-0 right-0 z-50 w-full justify-between items-center max-w-7xl mx-auto px-5 lg:px-0 py-4",
       )}
     >
-      <div className="isolate flex w-full justify-between items-center px-6 py-3 bg-white/10 backdrop-blur-lg border border-white/20 rounded-full shadow-lg ring-1 ring-black/5">
+      <div className="isolate flex w-full justify-between items-center px-6 py-3 bg-white/10 backdrop-blur-lg rounded-full shadow-lg ring-1 ring-black/5">
         <Logo />
         <PagesNav />
         <Navbar />
