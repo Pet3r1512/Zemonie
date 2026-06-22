852906ce2881d070d01e90df3027cd7f48839e33
Peter Pham <pttp15122002@gmail.com>
2026-06-22 22:22:57 +1000

    style: apply glass animation

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -9,12 +9,14 @@ export default function Header({ hideHeader }: { hideHeader: boolean }) {
       role="header"
       className={cn(
         hideHeader ? "hidden" : "flex",
-        "w-full justify-between items-center max-w-7xl mx-auto px-5 lg:px-0 py-10 lg:py-5",
+        "w-full justify-between items-center max-w-7xl mx-auto px-5 lg:px-0 py-4",
       )}
     >
-      <Logo />
-      <PagesNav />
-      <Navbar />
+      <div className="isolate flex w-full justify-between items-center px-6 py-3 bg-white/10 backdrop-blur-lg border border-white/20 rounded-full shadow-lg ring-1 ring-black/5">
+        <Logo />
+        <PagesNav />
+        <Navbar />
+      </div>
     </header>
   );
 }
