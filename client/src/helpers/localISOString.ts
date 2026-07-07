69935ad6624a2f0be4cc63719c49264650100a23
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-07-07 18:31:28 +1000

    #FIX-045: Create transaction with current time instead of midnight (#284)

--- client/src/helpers/localISOString.ts
+++ client/src/helpers/localISOString.ts
@@ -1,4 +1,7 @@
+const pad = (n: number) => String(n).padStart(2, "0");
+const now: Date = new Date();
+const hour: number = now.getHours();
+const minute: number = now.getMinutes();
 export default function localISOString(date: Date = new Date()): string {
-  const pad = (n: number) => String(n).padStart(2, "0");
-  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T00:00:00.000Z`;
+  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${hour}:${minute}:00.000Z`;
 }
