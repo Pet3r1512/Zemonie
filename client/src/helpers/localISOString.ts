f60a99f55282462fa4cf8b334c2be7d433b91c78
Peter Pham <pttp15122002@gmail.com>
2026-07-07 18:26:02 +1000

    imp: declare as global variable

--- client/src/helpers/localISOString.ts
+++ client/src/helpers/localISOString.ts
@@ -1,4 +1,4 @@
+const pad = (n: number) => String(n).padStart(2, "0");
 export default function localISOString(date: Date = new Date()): string {
-  const pad = (n: number) => String(n).padStart(2, "0");
   return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T00:00:00.000Z`;
 }
