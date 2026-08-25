dbed1a609feb1cc80a329c4077c8c1059846246b
Peter Pham <pttp15122002@gmail.com>
2026-08-25 21:32:20 +1000

    add: update password container to security section

--- client/src/components/Dashboard/Settings/Security/index.tsx
+++ client/src/components/Dashboard/Settings/Security/index.tsx
@@ -1,12 +1,14 @@
 import { Lock } from "lucide-react";
+import UpdatePassword from "./UpdatePassword";
 
 export default function Security() {
   return (
-    <section>
+    <section className="space-y-6">
       <div className="flex items-center gap-x-1 text-secondary">
         <Lock />
         <p className="lg:text-lg font-bold">Security</p>
       </div>
+      <UpdatePassword />
     </section>
   );
 }
