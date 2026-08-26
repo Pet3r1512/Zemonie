227dca1249d83308d39c2c8ba399e211329b4cb7
Peter Pham <pttp15122002@gmail.com>
2026-08-26 21:00:43 +1000

    style: for security section's title

--- client/src/components/Dashboard/Settings/Security/index.tsx
+++ client/src/components/Dashboard/Settings/Security/index.tsx
@@ -1,13 +1,9 @@
-import { Lock } from "lucide-react";
 import UpdatePassword from "./UpdatePassword";
 
 export default function Security() {
   return (
     <section className="space-y-6">
-      <div className="flex items-center gap-x-1 text-secondary">
-        <Lock />
-        <p className="lg:text-lg font-bold">Security</p>
-      </div>
+      <p className="lg:text-lg font-bold text-secondary">Account Security</p>
       <UpdatePassword />
     </section>
   );
