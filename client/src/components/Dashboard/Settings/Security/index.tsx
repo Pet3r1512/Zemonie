c81c762dff2d05d858b1eb5f664b5eec41219eba
Peter Pham <pttp15122002@gmail.com>
2026-08-26 21:16:13 +1000

    add: subtitle for update password form

--- client/src/components/Dashboard/Settings/Security/index.tsx
+++ client/src/components/Dashboard/Settings/Security/index.tsx
@@ -6,6 +6,7 @@ export default function Security() {
     <section className="space-y-6">
       <p className="lg:text-lg font-bold text-secondary">Account Security</p>
       <Separator className="dark:bg-white/15 md:max-w-xl lg:max-w-2xl" />
+      <p className="text-secondary font-semibold">Update Password</p>
       <UpdatePassword />
     </section>
   );
