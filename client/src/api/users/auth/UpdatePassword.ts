f78352aec499d6c6eaa0565ea71a1230767ac2a1
Peter Pham <pttp15122002@gmail.com>
2026-08-29 15:25:30 +1000

    imp: use different name

--- client/src/api/users/auth/UpdatePassword.ts
+++ client/src/api/users/auth/UpdatePassword.ts
@@ -1,7 +1,7 @@
 import { UpdatePassswordFormType } from "@/components/Dashboard/Settings/Security/UpdatePassword";
 import { authClient } from "@/lib/auth-client";
 
-export default async function UpdatePassword(credentials: UpdatePassswordFormType) {
+export default async function ChangePassword(credentials: UpdatePassswordFormType) {
   const { data, error } = await authClient.changePassword({
     currentPassword: credentials.currentPassword,
     newPassword: credentials.newPassword,
