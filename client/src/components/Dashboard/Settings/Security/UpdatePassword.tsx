6c3d4bd03bed53a7b8a81ba2eed5516fbf91fe71
Peter Pham <pttp15122002@gmail.com>
2026-08-29 14:47:58 +1000

    add: button to sent req to update password

--- client/src/components/Dashboard/Settings/Security/UpdatePassword.tsx
+++ client/src/components/Dashboard/Settings/Security/UpdatePassword.tsx
@@ -1,13 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { Field, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff, Save } from "lucide-react";
 import { useState } from "react";
 
 export default function UpdatePassword() {
   const [hideCurrentPassword, setHideCurrentPassword] = useState<boolean>(true);
   const [hideNewPassword, setHideNewPassword] = useState<boolean>(true);
   const [hideNewConfirmPassword, setHideNewConfirmPassword] = useState<boolean>(true);
+  const [allowToUpdate, setAllowToUpdate] = useState<boolean>(false);
 
   return (
     <div className="px-6 md:px-10  md:max-w-xl lg:max-w-2xl">
@@ -99,30 +101,10 @@ export default function UpdatePassword() {
               </button>
             </div>
           </Field>
-          <Field>
-            <Label
-              htmlFor="newPassword"
-              className="text-sm font-medium text-neutral-900 dark:text-neutral-100"
-            >
-              New Password
-            </Label>
-            <div className="relative">
-              <Input
-                id="newPassword"
-                type="text"
-                role="newPasswordInput"
-                className="h-10 bg-white dark:bg-dark-card/50 border-neutral-200 dark:border-dark-elevated text-neutral-900 dark:text-neutral-100 placeholder:text-neutral-400 dark:placeholder:text-neutral-500"
-              />
-              <button
-                tabIndex={-1}
-                type="button"
-                data-testid="newpassword-toggle"
-                className="absolute top-1/2 right-2.5 -translate-y-1/2"
-              >
-                <Eye />
-              </button>
-            </div>
-          </Field>
+          <Button disabled={!allowToUpdate} className="w-fit ml-auto">
+            <Save />
+            Update Password
+          </Button>
         </FieldGroup>
       </form>
     </div>
