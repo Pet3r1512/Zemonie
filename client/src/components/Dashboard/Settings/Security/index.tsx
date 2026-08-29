944d1ea007b2232a109b6fed37d2ef1c9ee309c4
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2026-08-29 21:21:56 +1000

    #FEAT-072: Only allow to update password every month (#312)

--- client/src/components/Dashboard/Settings/Security/index.tsx
+++ client/src/components/Dashboard/Settings/Security/index.tsx
@@ -1,13 +1,76 @@
 import { Separator } from "@/components/ui/separator";
 import UpdatePassword from "./UpdatePassword";
+import { authClient } from "@/lib/auth-client";
+import { useQuery } from "@tanstack/react-query";
+
+interface Account {
+  accountId: string;
+  providerId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+  id: string;
+  scopes: string[];
+}
 
 export default function Security() {
+  const accountsQuery = useQuery({
+    queryKey: ["accounts"],
+    queryFn: async () => {
+      const response = await authClient.listAccounts();
+      return response;
+    },
+    refetchOnWindowFocus: false,
+    staleTime: 3 * 60 * 1000,
+    gcTime: 30 * 60 * 1000,
+  });
+
+  if (!accountsQuery.data?.data || accountsQuery.data.data.length === 0) {
+    return;
+  }
+
+  const account = {
+    ...accountsQuery.data.data[0],
+    createdAt:
+      accountsQuery.data.data[0].createdAt instanceof Date
+        ? accountsQuery.data.data[0].createdAt.toISOString()
+        : accountsQuery.data.data[0].createdAt,
+    updatedAt:
+      accountsQuery.data.data[0].updatedAt instanceof Date
+        ? accountsQuery.data.data[0].updatedAt.toISOString()
+        : accountsQuery.data.data[0].updatedAt,
+  } as Account;
+
+  const createdAt = new Date(account.createdAt);
+  const updatedAt = new Date(account.updatedAt);
+
+  const nextUpdateAt = new Date(updatedAt);
+  nextUpdateAt.setUTCDate(nextUpdateAt.getUTCDate() + 30);
+
+  const isAllowedToChangePassword =
+    createdAt.getTime() === updatedAt.getTime() || new Date() >= nextUpdateAt;
+
   return (
     <section className="space-y-6">
       <p className="lg:text-lg font-bold text-secondary">Account Security</p>
+
       <Separator className="dark:bg-white/15 md:max-w-xl lg:max-w-2xl" />
+
       <p className="text-secondary font-semibold">Update Password</p>
-      <UpdatePassword />
+
+      {accountsQuery.isError && <p>Error: {accountsQuery.error.message}</p>}
+      {accountsQuery.isPending && <p>Loading...</p>}
+      {!accountsQuery.data?.data ||
+        (accountsQuery.data.data.length === 0 && <p>No accounts found.</p>)}
+
+      {isAllowedToChangePassword ? (
+        <UpdatePassword />
+      ) : (
+        <p>
+          You can update your password again on{" "}
+          <span className="text-primary font-bold">{nextUpdateAt.toLocaleDateString()}</span>.
+        </p>
+      )}
     </section>
   );
 }
