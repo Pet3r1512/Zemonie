b9047cb4dbf9fcf70b67373e3db4209fa32dee9f
Peter Pham <pttp15122002@gmail.com>
2026-09-05 10:57:33 +1000

    chore: remove unused

--- server/src/server/_index.ts
+++ server/src/server/_index.ts
@@ -2,7 +2,6 @@ import { analyticsRouter } from "./analytics";
 import { balancesRouter } from "./balance";
 import { budgetRouter } from "./budget";
 import { categoriesRouter } from "./categories";
-import { emailsRouter } from "./emails";
 import { transactionsRouter } from "./transactions";
 import { router } from "./tRPC";
 import { userRouter } from "./user";
@@ -14,7 +13,6 @@ export const appRouter = router({
   analytics: analyticsRouter,
   user: userRouter,
   budget: budgetRouter,
-  emails: emailsRouter,
 });
 
 export type AppRouter = typeof appRouter;
